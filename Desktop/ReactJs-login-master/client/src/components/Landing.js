18f295dec53eca912b24ab5f651bea6f8401c3be
RS1611 <salianriya@gmail.com>
2020-05-24 11:29:02 +0530

    first commit

---
+++ Desktop/ReactJs-login-master/client/src/components/Landing.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+class Landing extends Component {
+  render() {
+    return (
+      
+      <div className="container">
+        <div className="jumbotron mt-5">
+          <div className="col-sm-8 mx-auto">
+            <img src="WelcomeFolks.png" alt="Avatar" class="avatar1"></img>
+            <h1 className="text-center">WELCOME FOLKS!!!</h1>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Landing
